b6a4351305d4d616cb77abc4a0b28064d92d4785
Anthony <anthonyharisson30@gmail.com>
2022-12-20 01:06:11 -0300

    adding static information to the layout

--- src/screens/Tickets/index.js
+++ src/screens/Tickets/index.js
@@ -1,10 +1,14 @@
-import { SafeAreaView, Image, View, Pressable } from 'react-native';
-import React, { useContext, useEffect } from 'react';
+import { ScrollView, Image, View, Pressable, Dimensions, FlatList, Text, TouchableOpacity } from 'react-native';
+import React, { useContext } from 'react';
 import Header from '../../Components/Header'
 import { styles } from './styles';
 import Seats from '../../Data/Seats.json';
 import TicketStatus from '../../Components/TicketStatus';
 import { TicketContext } from '../../context/TicketContext';
+import Title from '../../Components/Title';
+import Date from '../../Data/Date.json';
+
+const hours = ["12:00", "13:00", "14:00", "15:00", "16:00"]
 
 export default function Tickets() {
 
@@ -24,13 +28,9 @@ export default function Tickets() {
     }
     
   }
-  
-  useEffect(()=>{
-
-  },[])
 
   return (
-    <SafeAreaView style={styles.container}>
+    <ScrollView style={styles.container}>
       <Header pagetitle='Select Seats' />
       <Image style={styles.imageCine} source={require('../../Assets/cine.png')} />
       <View style={styles.seatsContainer}>
@@ -43,6 +43,45 @@ export default function Tickets() {
           <TicketStatus color='#ffa500' status='Selected' />
           <TicketStatus color='white' status='Reserved' />
       </View>
-    </SafeAreaView>
+      <View>
+        <Title text='Select Date and Time' style={{fontSize: 17, alignSelf: 'center', marginVertical: 10}} />
+        <View style={{paddingHorizontal: 24, marginBottom: 50}}>
+          <FlatList
+            data={Date}
+            keyExtractor={(date) => String(date.day)}
+            horizontal
+            renderItem={({date, index}) => {
+              return(
+                <View style={styles.dateContainer}>
+                  <Text style={styles.textMonth}>Dec</Text>
+                  <View style={styles.circleDay}>
+                    <Text style={styles.textDay}>{index}</Text>
+                  </View>
+              </View>
+              )
+            }}
+          />
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          >
+            {hours.map( hour => (
+              <View key={hour} style={styles.hourContainer}>
+                <Text style={styles.textHour}>{hour}</Text>
+              </View>
+            ))}
+          </ScrollView>
+          <View style={styles.finishContainer}>
+            <View style={styles.priceContainer}>
+              <Text style={{color: 'white', fontWeight: '600', fontSize: 17}}>Total Price</Text>
+              <Text style={{color: 'white', fontWeight: '500', fontSize: 16}}>$32.16</Text>
+            </View>
+            <TouchableOpacity style={styles.btn}>
+              <Text style={styles.btnText}>Book Ticket</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
--- src/screens/Tickets/styles.js
+++ src/screens/Tickets/styles.js
@@ -34,5 +34,64 @@ export const styles = StyleSheet.create({
     justifyContent: 'center',
     marginVertical: 20,
     flexDirection: 'row'
+  },
+  dateContainer: {
+    width: 60,
+    height: 90,
+    backgroundColor: '#403d3d',
+    marginHorizontal: 5,
+    borderRadius: 27,
+    alignItems: 'center',
+  },
+  circleDay: {
+    width: 40,
+    height: 40,
+    backgroundColor: 'white',
+    borderRadius: 40,
+    marginTop: 8,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  textMonth: {
+    color: 'white',
+    fontWeight: '600',
+    marginTop: 10
+  },
+  textDay: {
+    fontWeight: '600'
+  },
+  hourContainer: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 60,
+    height: 40,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 20,
+    marginHorizontal: 10
+  },
+  textHour: {
+    color: 'white'
+  },
+  finishContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  priceContainer: {
+    alignItems: 'center'
+  },
+  btn: {
+    backgroundColor: '#ffa500',
+    width: width / 1.9,
+    height: 50,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnText: {
+    fontWeight: '700',
+    fontSize: 13
   }
 });
